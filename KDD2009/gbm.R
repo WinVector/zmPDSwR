81aa7a704363dfa6bb708d88968fb156057f68e6
John Mount <jmount@win-vector.com>
2014-01-31 08:59:16 -0800

    add some reasonable settings

--- KDD2009/gbm.R
+++ KDD2009/gbm.R
@@ -5,10 +5,12 @@
 load('KDD2009.Rdata')
 library('gbm')
 library('ROCR')
-gmodel <- gbm(as.formula(f),data=dTrain)
+
+gmodel <- gbm(as.formula(f),interaction.depth=5,dist='bernoulli',
+   data=dTrain)
 calcAUC(predict(gmodel,n.trees=100,newdata=dTrain),dTrain[,outcome])
-## [1] 0.6327771
-calcAUC(predict(gmodel,n.trees=100,newdata=dTest),dTest[,outcome])
-## [1] 0.645889
+## [1] 0.7008435
 calcAUC(predict(gmodel,n.trees=100,newdata=dCal),dCal[,outcome])
-## [1] 0.6246506
+## [1] 0.6793182
+calcAUC(predict(gmodel,n.trees=100,newdata=dTest),dTest[,outcome])
+## [1] 0.7064753
