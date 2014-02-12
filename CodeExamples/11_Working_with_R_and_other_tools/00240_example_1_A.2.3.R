cd907f1e6c4fd55754acf1a449dc9da92785c5cb
John Mount <jmount@win-vector.com>
2014-02-12 09:56:10 -0800

    remove some wide content

--- CodeExamples/11_Working_with_R_and_other_tools/00240_example_1_A.2.3.R
+++ CodeExamples/11_Working_with_R_and_other_tools/00240_example_1_A.2.3.R
@@ -13,12 +13,6 @@ mkCon <- function(nm) { 	# Note: 3
 }
 cars <- read.table(mkCon('UCICar/car.data.csv'), 	# Note: 4 
     sep=',',header=T,comment.char='')
-print(summary(cars))
-##    buying      maint       doors     persons     lug_boot    safety      rating    
-##  high :432   high :432   2    :432   2   :576   big  :576   high:576   acc  : 384  
-##  low  :432   low  :432   3    :432   4   :576   med  :576   low :576   good :  69  
-##  med  :432   med  :432   4    :432   more:576   small:576   med :576   unacc:1210  
-##  vhigh:432   vhigh:432   5more:432                                     vgood:  65
 
 # Note 1: 
 #   Bring in the RCurl library for more connection methods 
