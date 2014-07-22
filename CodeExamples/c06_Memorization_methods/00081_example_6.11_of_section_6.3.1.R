f60c3f5524426a7c43293d676a40071b4dff06e0
John Mount <jmount@win-vector.com>
2014-07-22 15:17:16 -0700

    put latest errata in listing (was not using entropy in variable selection, despite saying so)

--- CodeExamples/c06_Memorization_methods/00081_example_6.11_of_section_6.3.1.R
+++ CodeExamples/c06_Memorization_methods/00081_example_6.11_of_section_6.3.1.R
@@ -2,6 +2,27 @@
 # (example 6.11 of section 6.3.1)  : Memorization methods : Building models using many variables : Variable selection 
 # Title: Basic variable selection 
 
+# Each variable we use represents a chance of explaining more of the
+# outcome variation (a chance of building a better model) but also
+# represents a possible source of noise and overfitting. To control this
+# effect, we often preselect which subset of variables we’ll use to
+# fit. Variable selection can be an important defensive modeling step
+# even for types of models that “don’t need it” (as seen with decision
+# trees in section ).  Listing
+#  shows a hand-rolled
+# variable selection loop where each variable is scored according to a
+# deviance inspired score, where a variable is scored with a bonus
+# proportional to the change in in scaled log likelihood of the training
+# data.  We could also try an AIC (Akaike information criterion) by
+# subtracting a penalty proportional to the complexity of the variable
+# (which in this case is 2^entropy for categorical
+# variables and a stand-in of 1 for numeric variables).  The score is a
+# bit ad hoc, but tends to work well in selecting variables. Notice
+# we’re using performance on the calibration set (not the training set)
+# to pick variables. Note that we don’t use the test set for
+# calibration; to do so lessens the reliability of the test set for
+# model quality confirmation.
+
 logLikelyhood <- function(outCol,predCol) { 	# Note: 1 
   sum(ifelse(outCol==pos,log(predCol),log(1-predCol)))
 }
@@ -25,7 +46,7 @@ for(v in catVars) {  	# Note: 2
 for(v in numericVars) { 	# Note: 3 
   pi <- paste('pred',v,sep='')
   liCheck <- 2*((logLikelyhood(dCal[,outcome],dCal[,pi]) -
-      baseRateCheck) - 1)
+      baseRateCheck))
   if(liCheck>=minStep) {
      print(sprintf("%s, calibrationScore: %g",
         pi,liCheck))
