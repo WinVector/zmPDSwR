07b2cd9750e848418520da89276ff8c34855cf8e
John Mount <jmount@win-vector.com>
2015-12-10 15:34:42 -0800

    re-run

--- RunExamples/runDir.R
+++ RunExamples/runDir.R
@@ -1,17 +1,25 @@
 
-runDir <- function(sourcedir,datadir) {
+runDir <- function(sourcedir,datadir,first=0,last=1000000000) {
   origCurDir <- getwd()
   setwd(datadir)
-  for(sfile in sort(list.files(sourcedir))) {
-    spath <- paste(sourcedir,sfile,sep='/')
-    print('###############################')
-    print(paste("#####  running ",spath))
-    print(paste("#####   in directory",datadir))
-    source(spath,
-           echo=TRUE,print.eval=TRUE,keep.source=TRUE,
-           prompt.echo='> ',continue.echo=' ',
-           max.deparse.length=1000000000)
-    print('###############################')
+  for(sfile in sort(list.files(sourcedir,
+                               pattern='.*.R'))) {
+    exampleNumber <- as.integer(gsub('_.*$','',sfile))
+    if((exampleNumber>=first)&&(exampleNumber<=last)) {
+      spath <- paste(sourcedir,sfile,sep='/')
+      print(paste('############################### start ',
+                  exampleNumber,
+                  date()))
+      print(paste("#####  running ",spath))
+      print(paste("#####   in directory",datadir))
+      source(spath,
+             echo=TRUE,print.eval=TRUE,keep.source=TRUE,
+             prompt.echo='> ',continue.echo=' ',
+             max.deparse.length=1000000000)
+      print(paste('############################### end ',
+                  exampleNumber,
+                  date()))
+    }
   }
   setwd(origCurDir)
 }
