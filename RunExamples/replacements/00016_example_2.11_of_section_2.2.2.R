0972ad318dbebebedbb67f924c0d7673ec3e3c2c
John Mount <jmount@win-vector.com>
2015-12-12 17:25:16 -0800

    add H2/JDBC example

--- RunExamples/replacements/00016_example_2.11_of_section_2.2.2.R
+++
@@ -1,7 +0,0 @@
-
-# Step 00016_example_2.11_of_section_2.2.2.R requires large files that
-# we don't have the rights to redistributed in their entirety.  So we
-# are skipping that step and working from saved data.
-
-load('phsample.RData')
-
