10092fe0ef619f92df50a3202b3584a155c79363
John Mount <jmount@win-vector.com>
2019-05-07 06:46:30 -0700

    Issue 12

--- CodeExamples/c05_Choosing_and_evaluating_models/00070_example_5.14_of_section_5.2.5.R
+++ CodeExamples/c05_Choosing_and_evaluating_models/00070_example_5.14_of_section_5.2.5.R
@@ -3,14 +3,17 @@
 # Title: Calculating the typical distance between items in every pair of clusters 
 
 library('reshape2')
-n <- dim(d)[[1]]
-pairs <- data.frame(
-   ca = as.vector(outer(1:n,1:n,function(a,b) d[a,'cluster'])),
-   cb = as.vector(outer(1:n,1:n,function(a,b) d[b,'cluster'])),
-   dist = as.vector(outer(1:n,1:n,function(a,b)
-           sqrt((d[a,'x']-d[b,'x'])^2 + (d[a,'y']-d[b,'y'])^2)))
-   )
-dcast(pairs,ca~cb,value.var='dist',mean)
+
+n <- nrow(d)
+
+pairs <- merge(1:n, 1:n, all=TRUE)
+names(pairs) <- c("a",  "b")
+pairs$ca <- d[pairs$a, 'cluster'][[1]]
+pairs$cb <- d[pairs$b, 'cluster'][[1]]
+pairs$dist <- sqrt((d[pairs$a, 'x']-d[pairs$b, 'x'])^2 + (d[pairs$a, 'y']-d[pairs$b, 'y'])^2)[[1]]
+
+dcast(pairs, ca~cb, value.var='dist', mean)
+
 ##   ca         1         2         3         4         5
 ## 1  1 0.1478480 0.6524103 0.3780785 0.4404508 0.7544134
 ## 2  2 0.6524103 0.2794181 0.5551967 0.4990632 0.5165320
